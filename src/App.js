29f4d3702ef5cad4b820893da2d91b0ed5bb5bae
Giuliano Damico <giulianodamico2019@gmail.com>
2022-03-02 23:12:32 -0300

    Utilities and grid system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './sass/index.css';
 
 function App() {
   return (
@@ -7,15 +7,22 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          No manchen la historia, ganen o mueran
         </p>
+        <div className='bg-info font-sm container-sm'>
+          <div className='row'>
+            <div className='col-12 col-md-6 col-lg-3 bg-primary'>
+              <p>Putos: pongan huevos, o el pecho para las balas</p>
+            </div>
+          </div>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sports-images.vice.com/images/2016/04/20/untitled-article-1461167608-body-image-1461168053.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ganen o hay bala, venimos por el club, no por ustedes
         </a>
       </header>
     </div>
